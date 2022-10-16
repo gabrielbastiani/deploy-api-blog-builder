87ded90b0e6e4ce02f05e0f30816acae00079e05
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2022-10-15 21:25:07 -0300

    'deploy'

--- src/controllers/user/DeleteUserController.ts
+++ src/controllers/user/DeleteUserController.ts
@@ -1,26 +1,19 @@
 import { Request, Response } from "express";
 import { DeleteUserService } from "../../services/user/DeleteUserService";
-import { DeletePhotoUserService } from '../../services/user/DeletePhotoUserService'
-import fs from 'fs';
+
 
 class DeleteUserController {
   async handle(req: Request, res: Response) {
     const user_id = req.query.user_id as string;
 
     const deleteUserService = new DeleteUserService();
-    const removePhoto = new DeletePhotoUserService();
 
       const userPhoto = await deleteUserService.execute({
         user_id,
       });
 
-      fs.unlinkSync(__dirname + '/' + '..' + '/' + '..' + '/' + '..' + '/' + 'imgblog' + '/' + userPhoto.photo)
 
-      const user = await removePhoto.execute({
-        user_id
-      });
-
-      return res.json([userPhoto, user]);
+      return res.json(userPhoto);
     }
   }
 
